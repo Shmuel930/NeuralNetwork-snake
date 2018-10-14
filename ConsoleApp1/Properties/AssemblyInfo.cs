2d65b5ae0e1614321ea3c6974d20dacc692d1a84
Shmuel Cohen <ss9300@gmail.com>
2018-10-14 15:14:46 +0300

    Change app name from ConsoleApp to SnakeGame

--- ConsoleApp1/Properties/AssemblyInfo.cs
+++ ConsoleApp1/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("SnakeGame")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("SnakeGame")]
+[assembly: AssemblyCopyright("Shmuel Cohen Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
